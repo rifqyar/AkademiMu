5a9cc1b4cf5ae997187b125f3615f7f6f443e6be
= <=>
2022-09-21 16:28:17 +0700

    perbaikan splash screen

---
+++ src/view/pages/WellcomePages/WellcomePages.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Subheading } from 'react-native-paper'
+
+const WellcomePages = () => {
+  return (
+    <View >
+      <Subheading>Aplikasi Sistem Informasi Akademik STTM Muhammadiyah Cileungsi</Subheading>
+    </View>
+  )
+}
+
+export default WellcomePages
